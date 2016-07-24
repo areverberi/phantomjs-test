5efcfc7ec85ab5ce3f07271a426e4ada71f8dbb7
Andrea Reverberi <andrea.reverberi@gatech.edu>
2016-07-24 05:34:02 -0700

    endpoint now answers to get requests, some fixes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var phantom = require('phantom');
-var tmp = require('tmp');
+//var tmp = require('tmp');
 var tmpfile = require('tempfile');
 var fs = require('fs');
-
+//var nightmare = require('nightmare');
 /* GET home page. */
-router.post('/toPdf', function(req, res) {
+router.get('/toPdf', function(req, res) {
   var sitePage = null;
   var phInstance = null;
   var tmpFileName = null;
+  var requestArray = [];
   phantom.create()
     .then(function(instance) {
       phInstance = instance;
@@ -17,36 +18,56 @@ router.post('/toPdf', function(req, res) {
     })
     .then(function(page) {
       sitePage = page;
-      console.log(req.body.url);
-      console.log(req.body.token);
-      return page.open(req.body.url+'?token='+req.body.token+'&newsletter=true');
+      console.log(req.query.url);
+      console.log(req.query.token);
+      //return page.open(req.query.url+'?token='+req.query.token+'&newsletter=true');
+      //return sitePage.property('viewportSize', {width: 1920, height: 1600});
+    })
+    .then(function(property) {
+      sitePage.on('onResourceRequested', function(requestData, networkRequest) {
+        console.log('requestData', requestData);
+        console.log('networkRequest', networkRequest);
+        requestArray.push(requestData.id);
+      });
+      sitePage.on('onResourceReceived', function(response) {
+        console.log('response', response);
+        var index = requestArray.indexOf(response.id);
+        requestArray.splice(index, 1);
+      });
+      var reqUrl =req.query.url.indexOf('?') > -1 ? req.query.url+'&token='+req.query.token+'&newsletter=true' : req.query.url+'?token='+req.query.token+'&newsletter=true';
+      console.log(reqUrl);
+      return sitePage.open(req.query.url);
     })
     .then(function(status) {
       console.log(status);
-      var tmpFile = tmp.fileSync();
+      //var tmpFile = tmp.fileSync();
       tmpFileName = tmpfile('.pdf');
       console.log(tmpFileName);
-      return sitePage.render(tmpFileName);
-    })
-    .then(function(render) {
-      console.log(render);
-      phInstance.exit();
-      res.sendFile(tmpFileName, {}, function(err) {
-        if(err) {
-          console.log(err);
-          fs.unlink(tmpFileName);
-          return res.status(err.status).end();
-        }
-        fs.unlink(tmpFileName);
-      });
+      setTimeout(function() {
+        var interval = setInterval(function() {
+          if(requestArray.length === 0) {
+            clearInterval(interval);
+            console.log('clearing interval');
+            sitePage.render(tmpFileName)
+              .then(function(render) {
+                console.log('render result: ', render);
+                phInstance.exit();
+                return res.download(tmpFileName);
+              });
+
+          }
+        }, 10000);
+      }, 10000);
+
+
     })
     .catch(function(err) {
       console.log(err);
       phInstance.exit();
     });
 });
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
